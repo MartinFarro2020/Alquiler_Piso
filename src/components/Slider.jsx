5477cd8c7c5bdc579c0615612690b66de042cc78
martinfarro <martin.farro.martinez@gmail.com>
2025-02-26 02:43:53 +0100

    modify property

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,10 +4,35 @@ const Slider = () => {
         <div className="container">
             <div className="content">
                 <div className="bloqueuno">
-                    
+                    <h2>EN ALQUILER</h2>
                 </div>
                 <div className="bloquedos">
-                    <div className="bloquetres"></div>
+                    <div className="bloquetres">
+                        <h4>Piso en la calle no recuerdo 123 Ciudad</h4>
+                        <div className="images-logos">
+                            <div className="logos">
+                                <img src="/images/logo_cama_logo.png"/>
+                                <h3>4 Habs.</h3>
+                            </div>
+                            <div className="logos">
+                                <img src="/images/logo_baño_log.png"/>
+                                <h3>2 baños.</h3>
+                            </div>
+                            <div className="logos">
+                                <img src="/images/logo_metrocuadrado.png"/>
+                                <h3>80 m2.</h3>
+                            </div>
+                            <div className="logos">
+                                <img src="/images/logo_planta.png"/>
+                                <h3>2° planta</h3>
+                            </div>
+                            <div className="logos">
+                                <img src="/images/logo_asensor.png"/>
+                                <h3>Asensor</h3>
+                            </div>
+                            
+                        </div>
+                    </div>
                     <div className="bloquecuatro"></div>
                 </div>
             
