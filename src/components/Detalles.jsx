d4ae4302b7cd5ec5bfba8659dfd622c109ad620a
martinfarro <martin.farro.martinez@gmail.com>
2025-03-07 14:31:18 +0100

    botones

--- src/components/Detalles.jsx
+++ src/components/Detalles.jsx
@@ -2,8 +2,8 @@ const Detalles = () => {
   
     return(
         <div className="informacion">
-            <div>
-                <h1 className="title">Alquiler de Piso en Pueblo Mascarat-Altea-Alicante</h1>
+            <div className="inforamacion_detalle">
+                <h1>Alquiler de Piso en Pueblo Mascarat-Altea-Alicante</h1>
                 <h2>Direccion Calle Sorell n°9</h2>
                 <h3>ALQUILER POR LARGA TEMPORADA O CONDICIONES A CONVENIR DE MEDIA ESTANCIA</h3>
                 <h3>EN LA PLANTA 14 VISTAS INIGUALABLES</h3>
@@ -14,19 +14,29 @@ const Detalles = () => {
             <div className="pedirinfo">
                 <div className="mensaje">
                     <div className="boton_mensaje">Contactar
-                        <a className="" href="#Mail">
+                        <a href="#Mail">
                             <img className="logo_mensaje" src="/images/logomensaje1.png" alt="" />
                         </a>
                     </div>
                 </div>
+
                 <div className="pulso">
-                    <input type="submit" value="" className=""/>
+                    
                     <div className="wathsap-container">
                         <a href="https://wa.me/+34666860022?text=Quiero%20realizar%20un%20una consulta%20web" >
                             <img className="wathsap-icon" src="/images/logonegro.png" alt=""/>
                         </a>
                     </div>
-                </div>   
+                </div>
+
+                <div className="ubicacion">
+                    <div className="boton_ubicacion">
+                        <a href="#Map">
+                            <img className="logo_ubicacion" src="/images/ubicacion.png" alt="" />
+                        </a>
+                    </div>
+                </div>
+
             </div>    
         </div>    
     );
