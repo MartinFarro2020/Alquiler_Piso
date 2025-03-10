0378e4bccf77a87cf8e4344f8cf8ee42d35980ee
martinfarro <martin.farro.martinez@gmail.com>
2025-03-10 23:02:12 +0100

    space

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,7 +3,7 @@ const Map = () => {
     return(
         <div className="mapa_ubicacion" id="Map">    
             <div className="container_map">
-                <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3013.625687691732!2d-5.553187724873805!3d40.94587012326671!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd3f3b27f047dce7%3A0x802c95c894b64b04!2sC.%20Trasera%20de%20la%20Iglesia%2C%201%2C%2037181%20Calvarrasa%20de%20Abajo%2C%20Salamanca!5e0!3m2!1ses-419!2ses!4v1740163561191!5m2!1ses-419!2ses"></iframe>
+                <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3116.762920332053!2d0.001875674994521948!3d38.63133476236071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x129dff8b59d07d8f%3A0xad631ff1d4a9c99!2sC.%20Sorell%2C%209%2C%2003590%20Altea%2C%20Alicante!5e0!3m2!1ses!2ses!4v1741357473456!5m2!1ses!2ses"></iframe>
             </div>
             
         </div>
